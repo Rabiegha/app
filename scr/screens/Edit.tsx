f75ae47c587a748be0eca28a4fbd3b1e46db6a8f
RabyGha <80117176+Rabiegha@users.noreply.github.com>
2025-03-07 16:31:31 +0100

    edit component
    refactoring failed, but handling better the fileds errors, if the name, first name or email is not available the edit function is not executed

--- scr/screens/Edit.tsx
+++ scr/screens/Edit.tsx
@@ -1,45 +1,178 @@
-import React, { useEffect } from 'react';
-import { StatusBar, View } from 'react-native';
-import { useFocusEffect } from '@react-navigation/native';
-import { useSelector } from 'react-redux';
-import { selectCurrentUserId } from '../redux/selectors/auth/authSelectors.tsx';
-import { useEvent } from '../context/EventContext';
-import colors from '../assets/colors/colors';
-import globalStyle from '../assets/styles/globalStyle';
-import Spinner from 'react-native-loading-spinner-overlay';
-
+import React, {useContext, useEffect, useState} from 'react';
+import {StatusBar, StyleSheet, View} from 'react-native';
 import HeaderComponent from '../components/elements/header/HeaderComponent';
+import {useFocusEffect} from '@react-navigation/native';
+import globalStyle from '../assets/styles/globalStyle';
+import {useEvent} from '../context/EventContext';
+import axios from 'axios';
+import {BASE_URL} from '../config/config';
 import EditComponent from '../components/screens/EditComponent';
+import useUserId from '../hooks/useUserId';
+import colors from '../assets/colors/colors';
 import SuccessComponent from '../components/elements/notifications/SuccessComponent';
 import FailComponent from '../components/elements/notifications/FailComponent';
-
-import useEditAttendee from '../hooks/useEditAttendee';  // Custom hook for state & logic
+import {editAttendee} from '../services/editAttendeeService.tsx';
 import useAttendeeTypeDropdown from '../hooks/type/useAttendeeTypesDropdown.tsx';
+import {useSelector} from 'react-redux';
+import {selectCurrentUserId} from '../redux/selectors/auth/authSelectors.tsx';
+import Spinner from 'react-native-loading-spinner-overlay';
 
-const EditScreen = ({ navigation, route }) => {
-  useFocusEffect(() => {
-    StatusBar.setBarStyle('dark-content');
-    return () => {};
+const EditScreen = ({navigation, route}) => {
+  useFocusEffect(
+    React.useCallback(() => {
+      StatusBar.setBarStyle('dark-content');
+      return () => {};
+    }, []),
+  );
+  userId;
+  const [success, setSuccess] = useState(null);
+  const {secretCode, eventId, updateAttendee, triggerListRefresh} = useEvent();
+  const [attendeeTypes, setAttendeeTypes] = useState([]);
+  const userId = useSelector(selectCurrentUserId);
+  const {
+    attendeeId,
+    firstName,
+    lastName,
+    email,
+    phone,
+    organization,
+    jobTitle,
+    type,
+    typeId,
+  } = route.params;
+
+  const [nomModify, setNomModify] = useState(lastName);
+  const [prenomModify, setPrenomModify] = useState(firstName);
+  const [emailModify, setEmailModify] = useState(email);
+  const [numeroTelephoneModify, setNumeroTelephoneModify] = useState(phone);
+  const [societeModify, setSocieteModify] = useState(organization);
+  const [jobTitleModify, setJobTitleModify] = useState(jobTitle);
+  const [typeModify, setTypeModify] = useState(type);
+  const [typeIdModify, setTypeIdModify] = useState(typeId);
+  const [inputErrors, setInputErrors] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const resetFields = () => {
+    setInputErrors({});
+  };
+
+  const handleEnregistrer = async () => {
+    const errors = {};
+    setLoading(true);
+
+    // Validate each field and set errors
+    if (!nomModify) {
+      errors.nom = true;
+    }
+    if (!prenomModify) {
+      errors.prenom = true;
+    }
+    if (!emailModify) {
+      errors.email = true;
+    }
+
+    // Validate email format
+    if (emailModify) {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(emailModify)) {
+        errors.email = true;
+      }
+    }
+
+    // If there are errors, update the state and return
+    if (Object.keys(errors).length > 0) {
+      setInputErrors(errors);
+      setLoading(false);
+      setSuccess(false);
+      return;
+    }
+
+    // Logique pour traiter les données du formulaire
+    const attendeeData = {
+      send_confirmation_mail_ems_yn: 0,
+      generate_qrcode: 0,
+      generate_badge: 0,
+      send_badge_yn: 0,
+      // Plus d'options...
+      ems_secret_code: secretCode,
+      userId: userId,
+      salutation: '',
+      attendeeId: attendeeId,
+      first_name: prenomModify,
+      last_name: nomModify,
+      email: emailModify,
+      phone: numeroTelephoneModify,
+      organization: societeModify,
+      jobTitle: jobTitleModify,
+      typeId: typeIdModify,
+      status_id: '2',
+    };
+
+    try {
+      const response = await editAttendee(attendeeData);
+
+      if (response) {
+        console.log('Enregistrement réussi:');
+        setSuccess(true);
+        triggerListRefresh();
+        resetFields();
+        updateAttendee(eventId, {
+          id: attendeeId,
+          attendee_status: '2', // or the updated status
+          first_name: prenomModify,
+          last_name: nomModify,
+          email: emailModify,
+          phone: numeroTelephoneModify,
+          jobTitle: jobTitleModify,
+          type: typeModify,
+          typeId: typeIdModify,
+          event_id: eventId,
+        });
+      } else {
+        console.error('Enregistrement échoué:');
+        setSuccess(false);
+      }
+    } catch (error) {
+      console.error("Erreur lors de l'enregistrement:", error);
+      setSuccess(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useFocusEffect(
+    React.useCallback(() => {
+      return () => setSuccess(null);
+    }, []),
+  );
+  useEffect(() => {
+    console.log('attendeeId', attendeeId);
   });
 
-  const userId = useSelector(selectCurrentUserId);
-  const { eventId, updateAttendee, triggerListRefresh } = useEvent();
-  const dropdownOptions = useAttendeeTypeDropdown();
-
-  const {
-    attendeeData,
-    setAttendeeData,
-    success,
-    setSuccess,
-    inputErrors,
-    loading,
-    handleEnregistrer,
-    resetInputError,
-  } = useEditAttendee(route.params, userId, eventId, updateAttendee, triggerListRefresh);
-
   const handleGoBack = () => {
-    navigation.navigate('More', { ...attendeeData });
+    navigation.navigate('More', {
+      eventId: eventId,
+      attendeeId: attendeeId,
+      firstName: prenomModify,
+      lastName: nomModify,
+      email: emailModify,
+      phone: numeroTelephoneModify,
+      organization: societeModify,
+      jobTitle: jobTitleModify,
+      type: typeModify,
+      typeId: typeIdModify,
+    });
   };
+
+  const resetInputError = field => {
+    setInputErrors(prevErrors => {
+      const newErrors = {...prevErrors};
+      delete newErrors[field];
+      return newErrors;
+    });
+  };
+  // Use the custom hook
+  const dropdownOptions = useAttendeeTypeDropdown();
 
   return (
     <View style={globalStyle.backgroundWhite}>
@@ -47,21 +180,44 @@ const EditScreen = ({ navigation, route }) => {
         title="Modifier"
         color={colors.darkGrey}
         handlePress={handleGoBack}
-        backgroundColor="white"
+        backgroundColor={'white'}
       />
-
-      {success === true && <SuccessComponent onClose={() => setSuccess(null)} text="Modification enregistrée" />}
-      {success === false && <FailComponent onClose={() => setSuccess(null)} text="Modification non enregistrée" />}
-      <Spinner visible={loading} />
-
+      {success === true && (
+        <SuccessComponent
+          onClose={() => setSuccess(null)}
+          text={'Modification enregisreè'}
+        />
+      )}
+      {success === false && (
+        <FailComponent
+          onClose={() => setSuccess(null)}
+          text={'Modification non enregistrée'}
+        />
+      )}
+<Spinner visible={loading} />
       <EditComponent
         onPress={handleEnregistrer}
-        style={[globalStyle.container, { marginTop: 50 }]}
-        {...attendeeData}
+        style={[globalStyle.container, {marginTop: 50}]}
+        nom={nomModify}
+        prenom={prenomModify}
+        email={emailModify}
+        societe={societeModify}
+        jobTitle={jobTitleModify}
+        numeroTelephone={numeroTelephoneModify}
+        type={typeModify}
+        typeId={typeId}
         success={success}
         inputErrors={inputErrors}
         attendeeTypes={dropdownOptions}
-        setAttendeeData={setAttendeeData}
+        setNom={setNomModify}
+        setPrenom={setPrenomModify}
+        setEmail={setEmailModify}
+        setNumeroTelephone={setNumeroTelephoneModify}
+        setSociete={setSocieteModify}
+        setJobTitle={setJobTitleModify}
+        setSuccess={setSuccess}
+        setType={setTypeModify}
+        setTypeId={setTypeIdModify}
         resetInputError={resetInputError}
       />
     </View>
