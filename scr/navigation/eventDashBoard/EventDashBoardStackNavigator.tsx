1ea40180b75771b78d1574c4d704507681dc8a61
RabyGha <80117176+Rabiegha@users.noreply.github.com>
2025-04-01 11:42:34 +0200

    Creating event dash bord stack navigation
    To navigate to attendee list screen, and session screen

---
+++ scr/navigation/eventDashBoard/EventDashBoardStackNavigator.tsx
@@ -0,0 +1,23 @@
+// EventStackNavigator.js
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SessionListScreen from '../../screens/Session';
+import AllAttendeesListScreen from '../../screens/AllAttendeesListScreen';
+import EventDashboardTabsNavigator from './EventDashBoardTabsNavigator';
+
+const Stack = createNativeStackNavigator();
+
+export default function EventDashboardStackNavigator({ searchQuery }) {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {/* Tabs inside the stack */}
+      <Stack.Screen name="EventDashboard">
+        {() => <EventDashboardTabsNavigator searchQuery={searchQuery} />}
+      </Stack.Screen>
+
+      {/* Extra screens not in tabs */}
+      <Stack.Screen name="AllAttendeesList" component={AllAttendeesListScreen} />
+      <Stack.Screen name="SessionDetails" component={SessionListScreen} />
+    </Stack.Navigator>
+  );
+}
