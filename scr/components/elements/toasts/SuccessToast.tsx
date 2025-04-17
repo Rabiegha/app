c32c145b6d31979142bfd528697586e8bac52aaa
RabyGha <80117176+Rabiegha@users.noreply.github.com>
2025-04-17 15:01:04 +0200

    toast integration
    create toast config to use custumized toast components  create customized toasts

---
+++ scr/components/elements/toasts/SuccessToast.tsx
@@ -0,0 +1,87 @@
+import React, {useEffect, useRef} from 'react';
+import {
+  Animated,
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import Verifie from '../../../assets/images/icons/Verifie.png';
+import closeButton from '../../../assets/images/icons/closeButton.png';
+import colors from '../../../assets/colors/colors';
+
+const SuccessComponent = ({onClose, text1, text2, ...rest}) => {
+  const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value
+
+  useEffect(() => {
+    // Si isVisible est true, démarrez l'animation
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 500, // Durée plus courte pour un effet rapide
+      useNativeDriver: true,
+    }).start();
+  }, [fadeAnim]);
+
+  return (
+    <Animated.View style={[styles.notification, {opacity: fadeAnim}]}>
+      <View style={styles.textNotification}>
+        <Image
+          source={Verifie}
+          resizeMode="contain"
+          style={{
+            width: 15,
+            height: 15,
+            tintColor: colors.green,
+            marginRight: 10,
+          }}
+        />
+        <Text style={styles.buttonText}>{text1}</Text>
+        {text2 ? <Text style={styles.buttonText}>{text2}: </Text> : null}
+      </View>
+      <TouchableOpacity onPress={onClose} style={styles.closeButton}>
+        <Image source={closeButton} style={styles.buttonImage} />
+      </TouchableOpacity>
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  notification: {
+    position: 'absolute',
+    top: 70,
+    left: 0,
+    right: 0,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: colors.green,
+    backgroundColor: colors.lightGreen,
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    zIndex: 500,
+    marginHorizontal: 20,
+  },
+  textNotification: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: colors.green,
+    fontSize: 15,
+    fontWeight: 'bold',
+    textAlign: 'left',
+  },
+  closeButton: {},
+  buttonImage: {
+    width: 15,
+    height: 15,
+    tintColor: colors.green,
+    zIndex: 2,
+    marginLeft: 10,
+  },
+});
+export default SuccessComponent;
