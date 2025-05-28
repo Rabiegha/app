0b5216913bde60dc8754af4858e4d760c11be674
RabyGha <80117176+Rabiegha@users.noreply.github.com>
2025-05-28 18:59:36 +0200

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,49 @@
+// .eslintrc.js
 module.exports = {
   root: true,
-  extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+  },
+  env: {
+    browser: true,
+    node: true,
+    'jest/globals': true,
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'react-native',
+    'jest',
+    'import',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react-native/all',
+    'plugin:jest/recommended',
+    'plugin:import/typescript',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react-native/no-inline-styles': 'warn',
+    'import/order': ['warn', { 'newlines-between': 'always' }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
 };
