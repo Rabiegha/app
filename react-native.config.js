62fc2b1784608ab9270a41eaf5c97ace945bba53
RabyGha <80117176+Rabiegha@users.noreply.github.com>
2025-06-24 10:18:21 +0200

    debug

---
+++ react-native.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        ios: {},
+        android: {}
+      }
+    },
+    'react-native-linear-gradient': {
+      platforms: {
+        ios: {},
+        android: {}
+      }
+    }
+  },
+  assets: ['./node_modules/react-native-vector-icons/Fonts']
+};
